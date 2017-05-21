c27be7bad503919cb81f186ee158aae488b34ac9
andregema <andregputra@gmail.com>
2017-05-22 00:47:04 +0700

    trying to connect

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 use Herrera\Pdo\PdoServiceProvider;
-use Silex\Application as App;
+use Silex\Application;
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app = new App();
+$app = new Silex\Application();
 // $app->register(new Herrera\Pdo\PdoServiceProvider(),
 //                array(
 //                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
