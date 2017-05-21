fe3fa52e25b5256b55f1752ac563103b0c53cf57
andregema <andregputra@gmail.com>
2017-05-21 22:21:05 +0700

    test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $host = 'ec2-54-235-119-27.compute-1.amazonaws.com';
 $database = 'dadfe8kfs4eupc';
 $user = 'tmbuvfeidwknmz';
 $port = '5432';
-$password = 'c6874258cbfeea9626eeb3605e6179d7593fec1c2146def0f7c08f04097f3cb7'
+$password = 'c6874258cbfeea9626eeb3605e6179d7593fec1c2146def0f7c08f04097f3cb7';
 $url = 'postgres://tmbuvfeidwknmz:c6874258cbfeea9626eeb3605e6179d7593fec1c2146def0f7c08f04097f3cb7@ec2-54-235-119-27.compute-1.amazonaws.com:5432/dadfe8kfs4eupc';
 
 
