5b761b433312e7696a06a36fe837d56dd476c2f6
andregema <andregputra@gmail.com>
2017-05-22 00:16:12 +0700

    test

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
 
 
-$host = 'ec2-54-235-120-39.compute-1.amazonaws.com';
-$database = 'ddvr7bfki64vm9';
-$user = 'cwqcthugegdklr';
-$port = '5432';
-$password = '9c4deebbecf7ac3fbbd870dd269250b3e3e405097f5b73e23396ca75df26cce4';
-$url = 'postgres://cwqcthugegdklr:9c4deebbecf7ac3fbbd870dd269250b3e3e405097f5b73e23396ca75df26cce4@ec2-54-235-120-39.compute-1.amazonaws.com:5432/ddvr7bfki64vm9';
+// $host = 'ec2-54-235-120-39.compute-1.amazonaws.com';
+// $database = 'ddvr7bfki64vm9';
+// $user = 'cwqcthugegdklr';
+// $port = '5432';
+// $password = '9c4deebbecf7ac3fbbd870dd269250b3e3e405097f5b73e23396ca75df26cce4';
+// $url = 'postgres://cwqcthugegdklr:9c4deebbecf7ac3fbbd870dd269250b3e3e405097f5b73e23396ca75df26cce4@ec2-54-235-120-39.compute-1.amazonaws.com:5432/ddvr7bfki64vm9';
 
 
-$dbopts = parse_url(getenv('$url'));
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-               array(
-                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["$database"],'/').';host='.$dbopts["$host"] . ';port=' . $dbopts["$port"],
-                   'pdo.username' => $dbopts["$user"],
-                   'pdo.password' => $dbopts["$password"]
-               )
-);
+// $dbopts = parse_url(getenv('$url'));
+// $app->register(new Herrera\Pdo\PdoServiceProvider(),
+//                array(
+//                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["$database"],'/').';host='.$dbopts["$host"] . ';port=' . $dbopts["$port"],
+//                    'pdo.username' => $dbopts["$user"],
+//                    'pdo.password' => $dbopts["$password"]
+//                )
+// );
+
+
 
 echo("Za Worldeu4");
 
