204cf51bee00e9af2af0bf5bc55ffefb36680fb3
andregema <andregputra@gmail.com>
2017-05-22 00:35:13 +0700

    trying to connect to pg

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
+use Herrera\Pdo\PdoServiceProvider;
+use Silex\Application;
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
-// $app->register(new Herrera\Pdo\PdoServiceProvider(),
-//                array(
-//                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-//                    'pdo.username' => $dbopts["user"],
-//                    'pdo.password' => $dbopts["pass"]
-//                )
-// );
-echo($dbopts);
-echo("Za Worldeu5");
+$app = new Application();
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+               array(
+                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+                   'pdo.username' => $dbopts["user"],
+                   'pdo.password' => $dbopts["pass"]
+               )
+);
+
+echo("PG is Now Connectetd");
 
 ?>
