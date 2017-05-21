de828613a3db7759a051d45d5a8e85ff0355c36f
andregema <andregputra@gmail.com>
2017-05-22 00:39:44 +0700

    trying to connect

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ use Herrera\Pdo\PdoServiceProvider;
 use Silex\Application;
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
-$app = new Application();
-$app->register(new Herrera\Pdo\PdoServiceProvider(),
-               array(
-                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
-                   'pdo.username' => $dbopts["user"],
-                   'pdo.password' => $dbopts["pass"]
-               )
-);
+// $app = new Application();
+// $app->register(new Herrera\Pdo\PdoServiceProvider(),
+//                array(
+//                    'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+//                    'pdo.username' => $dbopts["user"],
+//                    'pdo.password' => $dbopts["pass"]
+//                )
+// );
 echo("PG is Now Connectetd");
 
 ?>
