347f0ee262226e4a4d7e70685116f8e7af148900
andregema <andregputra@gmail.com>
2017-05-24 19:05:51 +0700

    pgconnect test

---
+++ dbconnect.php
@@ -0,0 +1,13 @@
+<?php
+	$dbname = "ddvr7bfki64vm9";
+	$password = "9c4deebbecf7ac3fbbd870dd269250b3e3e405097f5b73e23396ca75df26cce4";
+	$host = "ec2-54-235-120-39.compute-1.amazonaws.com";
+	$user = "cwqcthugegdklr";
+
+	$connection = pg_connect ("host=$host dbname=$dbname user=$user password=$password");
+    if(!$connection) {
+      echo 'Error connecting to database';
+   	}
+   	$querySET = "SET SEARCH_PATH TO SIRIMA";
+   	pg_query($connection, $querySET);
+?>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<?php 
+    include("dbconnect.php");
+?>
 <html>
 	<head>
 		<meta charset="UTF-8">
