4da5cbde832b0e2009efbd83923c2d98749311ad
ajishoko <yudist1997@gmail.com>
2017-05-27 09:19:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,75 +1,118 @@
-<?php 
-    include("dbconnect.php");
-    $sql = "SELECT * FROM FORSA.AKUN";
-/*    $result = pg_query($connection , $sql);
-                            while($row = pg_fetch_array($result)){
-                                echo "  <tr>
-                                            <td>".$row[0]."</td>
-                                            <td>".$row[1]."</td>
-                                            <td>".$row[2]."</td>
-                                        </tr>";
-                            }*/
-?>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>F O R S A | Forum Mahasiswa</title>
-		<link rel="stylesheet" href="libs/bootstrap/dist/css/bootstrap.min.css">
-		
-	</head>
-	<body>
-		<nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>F O R S A | Forum Mahasiswa</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="libs/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="src/css/mycss.css">
+        <script type="text/JavaScript" src="src/js/jquery-3.1.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <!--insert more css file here -->
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-fixed-top" id='navbarFORSA'>
+            <div class='container-fluid'>
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="beranda.html"  id="websiteName">
-                    	<img src='img/forsared.png' height='35px' id='logoF' style='margin-top:-8px;'>
-                    </a>
-                </div>
-            	<ul class="nav navbar-nav">
-			      	<li class="active"><a href="beranda.html">Utama</a></li>
-					<li><a href="forum-portal.html">Forum</a></li>
-			    </ul>
-                 <script type="text/javascript">
-                    function pindah()
-                    {
-                        var username = document.getElementById("username").value;
-                        var password = document.getElementById("password").value;
-                        if(username=="username00" && password=="password"){
-                            alert("Login Sukses, klik button OK untuk dialihkan ke halaman Anda.");
-                            window.location.href="beranda.html";
-                        }
-                        else{
-                            alert("Login Gagal, silahkan periksa username atau password anda.");
-                        }
-                    }
-                </script>
-                <form class="navbar-form navbar-right">
-                    <form class="navbar-form navbar-right" method="post" action="./index.php">
-                        <div class="form-group">
-                            <input class="form-control input-sm" type="text" name="username" id="username" placeholder="username">
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control input-sm" type="password" name="password" id="password" placeholder="password">
-                        </div>
-                        <div class="form-group">
-                            <input type="button" class='btn btn-danger' value="Masuk" id="tombol-login" onclick="pindah()">
-                        </div>
-                    </form>
-                </form>
+                    <a href="index.php"><img src='img/official.png' height='35px' id='logoF' style='margin-top: 8px;margin-left: 8px;'></a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">F O R S A . I D</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="daftar.php"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
+                    <li><a href="#" data-toggle="modal" data-target="#myModal"><span class="glyphicon glyphicon-log-in"></span> Masuk</a></li>
+                </ul>
             </div>
         </nav>
-        <img src='img/jumboforsa01.png' class="img-responsive">
-        <img src='img/img2.png' class="img-responsive">
-        <div class="footer" id="footerWeb" style='text-align: center;'>
-            <hr>
-            <h4><b>&copy; 2017 - Continental Team</b></h4>
-            <br>
-            <h6>Alham Febian Rinaldy / 1506757636</h6>
-            <h6>Andre Gema Syahputra / 1506689282</h6>
-            <h6>Irfin Handiliastawan / 1506689704</h6>
-            <h6>Yosua Bisma Putrapratama / 1506689622</h6>
-            <h6>Yudistira Ramadhan / 1506689635</h6>
-           	<br>
+        <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title"><span><img src='img/official.png' height='35px'></span><b> Masuk ke Akun Anda</b></h5>
         </div>
-	</body>
+        <div class="modal-body">
+          <form method="post" action="./index.php">
+                <h6>Username</h6>
+                <input class="form-control" type="text" name="username" id="username" placeholder="Ketikkan Username Anda">
+                <br>
+                <h6>Password</h6>
+                <input class="form-control" type="password" name="password" id="password" placeholder="Ketikkan Password Anda">
+                <br>
+                <button type='submit' class='btn btn-danger' name='login' value='login' id="loginButton"><span class='glyphicon glyphicon-log-in'></span>  Masuk</button>
+          </form>
+          
+        </div>
+        <div class="modal-footer">
+            <?php
+                if(isset($_POST["login"])){
+                    $username = $_POST['username'];
+                    $password = $_POST['password'];
+                    if($username == "" && $password == ""){
+                        print"  <script>
+                                    alert('Harap isi Username atau Password anda untuk melanjutkan ke halaman FORSA');
+                                </script>";
+                    }
+                }
+            ?>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+        <div id="loginDisplay">
+            <div class='selamatdatang'>
+                <div id='div1' class='container'class='row'>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <div class='col-sm-6'>
+                        <h1 style='color:#fff' id='hai'><b>Selamat Datang di FORSA.ID</b></h1>
+                        <br>
+                        <br>
+                        <h4 style='color:#fff;' id='adalah'>FORSA.ID adalah tempat bagi para mahasiswa untuk berbagi ide, wawasan, dan opini dengan/bersama mahasiswa lainnya.</h4>
+                        <br>
+                        <br>
+                        <br>
+                        <h5 style='color:#fff;' id='akun'>Belum punya akun di FORSA.ID ?</h5>
+                        <br>
+                        <a href="daftar.php"><button type='submit' class='btn btn-danger' name='daftar' value='daftar' id="button">
+                            <h5>Buat Akun Sekarang</h5>
+                        </button></a>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                    </div>
+                    <div class='col-sm-6'>
+                    </div>
+                </div>
+            </div>
+            <div id='div2'>
+                <img class="img-responsive" src="img/jumboforsa02.jpg">
+            </div>
+            <div id='div3'>
+                <img class="img-responsive" src="img/img2.png">
+            </div>
+            
+        </div>
+        <div id='div4'>
+            <?php
+                include"footer.php";
+            ?>
+        </div>
+    </body>
 </html>
